2272dad60025f4e37eebe7bba6fc88c2e6547f6b
preferencej <preferencej@163.com>
2022-04-12 21:24:47 +0800

    second commit

--- src/main/java/com/atguigu/git/getTest.java
+++ src/main/java/com/atguigu/git/getTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class getTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test1");
     }
 }
