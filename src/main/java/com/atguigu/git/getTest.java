785aac2889829b559af16402e6c6733fba0d29dc
perferencej <103420162+perferencej@users.noreply.github.com>
2022-04-12 22:10:11 +0800

    pull test

--- src/main/java/com/atguigu/git/getTest.java
+++ src/main/java/com/atguigu/git/getTest.java
@@ -10,5 +10,6 @@ public class getTest {
         System.out.println("hollo test2");
         System.out.println("hollo test3");
         System.out.println("hollo test4");
+        System.out.println("hollo test5");
     }
 }
